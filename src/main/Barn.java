813cd99c3e10e8af567326e549971156f0fcac5d
dolph <dolph@172.16.26.245>
2015-05-07 12:19:56 +0200

    Udvidet barns getter/setter (skal have det hentet fra database, reelt)

--- src/main/Barn.java
+++ src/main/Barn.java
@@ -1,18 +1,24 @@
 package main;
 
 public class Barn {
+		private int ID;
 		private int alder = 0;
 		private String adresse = "";
 		private int saldo = 0;
-	public Barn(int alder, String adresse, int saldo ) {
+		private int kontaktNr;
+		private String navnPåDør;
+		
+	public Barn(int ID, int alder, int kontaktNr, String adresse, String navnPåDør, int saldo ) {
+		this.ID = ID;
+		this.kontaktNr = kontaktNr;
 		this.alder = alder;
 		this.adresse = adresse;
+		this.navnPåDør = navnPåDør;
 		this.saldo = saldo;
 	}
 	
 	public void setAlder(int alder) {
 		this.alder = alder;
-		
 	}
 	public int getAlder(){
 		return alder;
@@ -22,6 +28,27 @@ public class Barn {
 	}
 	public String getAdresse(){
 		return adresse;
+	}
+	public void setID(int ID){
+		this.ID = ID;
+	}
+	
+	public int getID(){
+		return ID;
+	}
+	public void setNavnPåDør(String navnPåDør){
+		this.navnPåDør = navnPåDør;
+	}
+	
+	public String getNavnPåDør(){
+		return navnPåDør;
+	}
+	public void setKontaktNr(int kontaktNr){
+		this.kontaktNr = kontaktNr;
+	}
+	
+	public int getKontaktNr(){
+		return kontaktNr;
 	}
 	public void setSaldo(int saldo){
 		this.saldo = saldo;
