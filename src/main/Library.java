940fe4a36ae72e439a9ae0a79519db02e2e5bc15
dolph <dolph@172.16.26.245>
2015-05-07 10:40:28 +0200

    Fixed lols i main

--- src/main/Library.java
+++ src/main/Library.java
@@ -4,7 +4,13 @@ public class Library {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+	}
+	
+	public static void lols(int x) { 
+		int i = 0; 
+		x = i;
 	}
 
 }
