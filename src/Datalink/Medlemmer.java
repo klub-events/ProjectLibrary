abf196a1df275641089513127999e78e3683d713
dolph <dolph@dolph-PC>
2015-05-11 00:12:30 +0200

    Getter/setter for medlemmer

--- src/Datalink/Medlemmer.java
+++ src/Datalink/Medlemmer.java
@@ -9,9 +9,9 @@ int fødselsdato;
 int telefon;
 String email;
 String navnPåDør;
-Boolean billeder;
+boolean billeder;
 
-	public Medlemmer(int id, String fornavn, String efternavn, String adresse, int fødselsdato, int telefon, String email, String navnPåDør, Boolean billeder){
+	public Medlemmer(int id, String fornavn, String efternavn, String adresse, int fødselsdato, int telefon, String email, String navnPåDør, boolean billeder){
 		this.id = id;
 		this.fornavn = fornavn;
 		this.efternavn = efternavn;
@@ -21,6 +21,61 @@ Boolean billeder;
 		this.email = email;
 		this.navnPåDør = navnPåDør;
 		this.billeder = billeder;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setFornavn(String fornavn) {
+		this.fornavn = fornavn;
+	}
+	public String getFornavn() {
+		return fornavn;
+	}	
+	public void setEfternavn(String efternavn) {
+		this.efternavn = efternavn;
+	}
+	public String getEfternavn() {
+		return efternavn;
+	}	
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getAdresse() {
+		return adresse;
+	}	
+	public void setFødselsdato(int fødselsdato) {
+		this.fødselsdato = fødselsdato;
+	}
+	public int getFødselsdato() {
+		return fødselsdato;
+	}	
+	public void setTelefon(int telefon) {
+		this.telefon = telefon;
+	}
+	public int getTelefon() {
+		return telefon;
+	}	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail() {
+		return email;
+	}	
+	public void setNavnPåDør(String navnPåDør) {
+		this.navnPåDør = navnPåDør;
+	}
+	public String getNavnPåDør() {
+		return navnPåDør;
+	}	
+	public void setBilleder(boolean billeder) {
+		this.billeder = billeder;
+	}
+	public boolean getBilleder() {
+		return billeder;
 	}
 }
 
