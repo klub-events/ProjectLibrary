8bbc5892cd90c77ea72101d0a7200951e248b180
Patrick <Patrick@192.168.0.12>
2015-05-30 21:57:38 +0200

    knap fix. javadoc. jtable fix. kode står kniv skarpt

--- src/GUI/AktivitetGUI.java
+++ src/GUI/AktivitetGUI.java
@@ -17,18 +17,20 @@ import javax.swing.JTable;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
-/** hvad extender den. implementere . bruges
+/** Denne klasse extender MainGUI som er den alle sider har tilfælles og 
+ * implementer Actionlistener får at knapper kan have funktionalitt.
+ * Indeholder knapper og tekstfields og jtable
  * @author Patrick
  *
  */
 public class AktivitetGUI extends MainGUI implements ActionListener
 {
+	//fields
 	private JButton btn_opret = new JButton("OPRET");	
-	
 	private JTable table;
 	private ClosedCellTableModel model = new ClosedCellTableModel();
 	private TableRowSorter<TableModel> rowSorter;
-	
+
 
 	private String navn;
 
@@ -45,156 +47,164 @@ public class AktivitetGUI extends MainGUI implements ActionListener
 	private LabelTextfield deltagerantalBox;
 
 	private LabelTextfield datoBox;
-	
+
 	public AktivitetGUI()
 	{
-		
+
 		//JPanel Labels
 		JPanel content1 = new JPanel(new GridLayout(10, 0));
-		
+
 		//Navne ud for textfield
 		String[] labelTekster = {"navn:", "pris:", "deltagerantal:", "dato:"};
-		
+
 		//adder navn og tekstfield til vinduet
 		this.navnBox = new LabelTextfield(labelTekster[0]);
 		String navn = navnBox.getInputText();
 		content1.add(navnBox);
-		
+
 		this.prisBox = new LabelTextfield(labelTekster[1]);
 		String pris = prisBox.getInputText();
 		content1.add(prisBox);
-		
+
 		this.deltagerantalBox = new LabelTextfield(labelTekster[2]);
 		String deltagerantal = deltagerantalBox.getInputText();
 		content1.add(deltagerantalBox);
-		
+
 		//speciel desginet dato api kommer
 		this.datoBox = new LabelTextfield(labelTekster[3]);
 		String dato = datoBox.getInputText();
-		content1.add(datoBox);
-		
-		
-		
-		Panel_Content.add(content1);
+
 		content1.setBackground(Color.WHITE);
+
 		btn_aktivitet.setBackground(Color.GRAY);
-		
+
 		btn_opret.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		btn_opret.addActionListener(this);
-		content1.add(btn_opret);
-		
-		        // Jtable
-				JScrollPane scrollPane = new JScrollPane();
-				scrollPane.setBounds(0, 0, 250, 200); // x, y, width, height
-				table = new JTable();
-				scrollPane.setViewportView(table);
-				table.setRowHeight(20);
-				table.setFillsViewportHeight(true);
-				table.setModel(model);
-				Panel_Content.add(scrollPane);
-				rowSorter = new TableRowSorter<>((table.getModel()));
-				table.setRowSorter(rowSorter);
 
-				rowSorter = new TableRowSorter<>((table.getModel()));
-				table.setRowSorter(rowSorter);
-			    
-				updateJTable();
-		
-		
+
+
+		// Jtable
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 250, 200); // x, y, width, height
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setRowHeight(20);
+		table.setFillsViewportHeight(true);
+		table.setModel(model);
+		rowSorter = new TableRowSorter<>((table.getModel()));
+		table.setRowSorter(rowSorter);
+		rowSorter = new TableRowSorter<>((table.getModel()));
+		table.setRowSorter(rowSorter);
+		updateJTable();
+
+		//adder
+		Panel_Content.add(scrollPane);
+		content1.add(datoBox);
+		Panel_Content.add(content1);
+		content1.add(btn_opret);
+
+
 	}//constructor slutter
 
-	public void updateJTable() {
+	/**
+	 * adder database udtræk til jtable
+	 */
+	public void updateJTable() 
+	{
 		ArrayList<Aktivitet> aktiviteter  = new Control().hentAktiviteter();
 		// add the column names
 		model.setColumnIdentifiers(new String[] { "id", "navn", "pris", "alder", "dato"});
 
-		// Foreach loop to loop through the ArrayList. One row (person) at a
-		// time
-		for (Aktivitet aktivitet : aktiviteter) {
+		//looper aktiviter igennem og indsætter i jtable
+		for (Aktivitet aktivitet : aktiviteter) 
+		{
 			model.addRow(new Object[]
 					{
-					
+
 					aktivitet.getid(), aktivitet.getNavn(), aktivitet.getPris(), aktivitet.getAntal(), aktivitet.getDato() 
-					
-					
-					
+
+
+
 					});	
 		}
-			
-		
 
-		//til patrick
-		//for (Medlem medlem : medlemmer){
-		//arrayList.add(medlem.getFornavn();
-		//}
 
-		//JCombobox aktivitetsMedlemmer(arrayList);
-		// add the DefaultTableModel to the JTable
-		
-	}
-	
-	
-	
+
+	}//update jtable slutter
+
+
+
 	/**
-	 *  parammetre returner, hvad gør den hvor bruges den
+	 *  Efter gemt indhold sørger denne metode for at tekstfieldsne er tomme bagefter ved at sætte dem = null
 	 */
-	public void clearAll() {
+	public void clearAll() 
+	{
 		navnBox.setText(null);
 		prisBox.setText(null);
 		deltagerantalBox.setText(null);
 		datoBox.setText(null);
-		
+
 	}
 
 
-	public boolean validateInput() {
-		if 		(  !navnBox.getInputText().equals("")
+	/**
+	 * Validere om der indhold i tekstfieldsne
+	 */
+	public boolean validateInput() 
+	{
+		if(  !navnBox.getInputText().equals("")
 				&& !prisBox.getInputText().equals("")
 				&& !deltagerantalBox.getInputText().equals("")
 				&& !datoBox.getInputText().equals(""))
-				 {
+		{
 			return true;
-		} else {
+		}
+		else 
+		{
 			return false;
 		}
 	}
-	public void actionPerformed(ActionEvent e)
+
+
+	/* (non-Javadoc)
+	 * @see GUI.MainGUI#actionPerformed(java.awt.event.ActionEvent)
+	 * Indeholder actionlistener til at indsætte i db
+	 */
+	public void actionPerformed(ActionEvent e) 
 	{
 		if(e.getSource() == btn_opret)
 		{
-			
+
 			navn = navnBox.getInputText();
 			pris = prisBox.getInputText();
 			antal = deltagerantalBox.getInputText();
 			dato = datoBox.getInputText();
-			
-			if(validateInput()){
-			
-				
-				
+
+			if(validateInput())
+			{
+
+
+
 				Aktivitet aktivitet = new Aktivitet(0, navn, pris, antal, dato);
 				new Control().opretAktivitet(aktivitet);
 				JOptionPane.showMessageDialog(frame,"Aktivitet oprettet!");
 
 				clearAll();
 			}
-			
+
 			else{
 				JOptionPane.showMessageDialog(frame,"Et eller flere felter er ikke blevet udfyldt. Udfyld alle felter, og prøv igen.");
 			}
-			
-				
-            new AktivitetGUI();
-			
+
+
+			new AktivitetGUI();
+
 			frame.dispose();
-									
 
-			}		
-			
-			
-		
 
-		
+		}		
+
+
 		if(e.getSource() == btn_aktivitet)
 		{
 			new AktivitetGUI();
@@ -207,28 +217,22 @@ public class AktivitetGUI extends MainGUI implements ActionListener
 			frame.dispose();
 		}
 
-		
-		if(e.getSource() == btn_saldo){
-			new SaldoGUI();
 
-			frame.dispose();
-		}
-
-		if(e.getSource() == btn_medlem )
+		if(e.getSource() == btn_saldo)
 		{
-			new MedlemGUI();
+			new SaldoGUI();
 			frame.dispose();
+
+
 		}
+
 
 		if(e.getSource() == btn_tilmeld)
 		{
 			new TilmeldAktivitetGUI();
 			frame.dispose();
 		}
-		if(e.getSource() == btn_saldo){
-			new SaldoGUI();
-			frame.dispose();
-		}
+
 		if(e.getSource() == btn_barbog )
 		{
 			new BarBogGUI();
@@ -236,7 +240,7 @@ public class AktivitetGUI extends MainGUI implements ActionListener
 		}		
 
 	}//actionperformed sluter
-	
+
 }//public class medlem_GUI slutter
 
 
